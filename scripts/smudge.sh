a86372ed19827830de45a3b1906166aa643863ef
mrmrwinter <mrmrwinter@gmail.com>
2020-03-10 12:49:30 +0000

    smudge, probably still wont work

--- scripts/smudge.sh
+++ scripts/smudge.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-L=$(smudgeplot.py cutoff {snakemake.input[0]} L)
-U=$(smudgeplot.py cutoff {snakemake.input[0]} U)
+L=$(smudgeplot.py cutoff reports/kmc/Mhaplanaria/kmer_k21.hist L)
+U=$(smudgeplot.py cutoff reports/kmc/Mhaplanaria/kmer_k21.hist U)
 
 echo $L $U # these need to be same values
 
 # L should be like 20 - 200
 # U should be like 500 - 3000
 
-kmc_tools transform reports/kmc/{snakemake.params[0]}/kmer_counts \
+kmc_tools transform reports/kmc/Mhaplanaria/kmer_counts \
 -ci$L \
 -cx$U dump \
--s reports/smudge/{snakemake.params[0]}/kmer_k21.dump
+-s reports/smudge/Mhaplanaria/kmer_k21.dump
 
 smudgeplot.py hetkmers \
--o reports/smudge/{snakemake.params[0]}/kmer_pairs < kmer_k21.dump \
+-o reports/smudge/Mhaplanaria/kmer_pairs < kmer_k21.dump \
 
 
-smudgeplot.py plot reports/smudge/{snakemake.params[0]}/kmer_pairs_coverages.tsv
+smudgeplot.py plot reports/smudge/Mhaplanaria/kmer_pairs_coverages.tsv
